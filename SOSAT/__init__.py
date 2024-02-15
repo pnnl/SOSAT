3fa7fb14a35ff9cf73f9bb652683f6fc071b3acf
wenjing.wang@pnnl.gov <wang679@we49753.pnl.gov>
2024-02-15 10:56:48 -0800

    Bump version: 0.7.0 → 0.7.1

--- SOSAT/__init__.py
+++ SOSAT/__init__.py
@@ -10,7 +10,7 @@ a probability density function for the principal stress components.
 
 __author__ = """Jeff Burghardt"""
 __email__ = 'jeffrey.burghardt@pnnl.gov'
-__version__ = '0.7.0'
+__version__ = '0.7.1'
 
 from .stress_state import StressState, units
 from .constraints import FaultConstraint
